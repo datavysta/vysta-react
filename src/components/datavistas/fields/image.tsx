a87c8ca113374bb1264a6f45cf325c9c04ee8cc0
jpfortier <jpfortier@gadzugo.com>
2025-01-17 13:52:23 -0600

    Restore image control

--- src/components/datavistas/fields/image.tsx
+++ src/components/datavistas/fields/image.tsx
@@ -1,24 +1,40 @@
 import { FC } from 'react';
 import { Image } from '@mantine/core';
 import IFieldProperty from '../../Models/public/fieldproperty';
+import DataType from '../../Models/DataType';
+
+const getContentType = (fileName?: string): string => {
+	if (!fileName) {
+		return 'image/png';
+	}
+
+	const parts = fileName.toLowerCase().split('.');
+	const extension = parts[parts.length - 1];
+	return `image/${extension}`;
+};
 
 const ImageComponent: FC<IFieldProperty> = ({
 	readOnly,
+	dataType,
 	value,
-	width,
-	height,
-	fit = 'contain'
+	fileName
 }) => {
-	if (!value) return null;
+	if (!value) {
+		return null;
+	}
 
-	return (
-		<Image
-			src={value}
-			width={width}
-			height={height}
-			fit={fit}
-		/>
-	);
+	if (readOnly) {
+		let src = value;
+
+		if (dataType === DataType.Binary) {
+			const contentType = getContentType(fileName);
+			src = `data:${contentType};base64,${value}`;
+		}
+
+		return <Image src={src} />;
+	}
+
+	return <>{value}</>;
 };
 
 export default ImageComponent;
